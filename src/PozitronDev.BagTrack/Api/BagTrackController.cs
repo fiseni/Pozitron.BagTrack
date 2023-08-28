fa4c4fedecaffb8688e06146dd2e701bad589191
Fati Iseni <fati.iseni@pozitrongroup.com>
2023-08-28 21:00:04 +0200

    Added azure-build.yml

--- src/PozitronDev.BagTrack/Api/BagTrackController.cs
+++ src/PozitronDev.BagTrack/Api/BagTrackController.cs
@@ -7,6 +7,11 @@ namespace PozitronDev.BagTrack.Api;
 [ApiController]
 public class BagTrackController : ControllerBase
 {
+    [HttpGet("bagmanager/services/tracking/arrTracking")]
+    [SwaggerOperation(Summary = "Get Bag information", Tags = new[] { "Arrival Tracking" })]
+    public async Task<ActionResult<BagDto>> List(string bagTrackId, [FromServices] IMediator mediator, CancellationToken cancellationToken)
+        => Ok(await mediator.Send(new BagGetRequest(bagTrackId), cancellationToken));
+
     [HttpPost("bagmanager/services/tracking/arrTracking")]
     [SwaggerOperation(Summary = "Add Bag information", Tags = new[] { "Arrival Tracking" })]
     public async Task<ActionResult<BagDto>> List(BagCreateDto bagCreateDto, [FromServices] IMediator mediator, CancellationToken cancellationToken)
